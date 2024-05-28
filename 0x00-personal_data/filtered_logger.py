e5eef6b97fbe8494cbda062d468acd1385e85624
NgigiN <ngigi.nyongo@gmail.com>
2024-05-28 13:01:03 +0300

    1-Logging_formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,4 +34,5 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         original_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION, original_message,
+                            self.SEPARATOR)
