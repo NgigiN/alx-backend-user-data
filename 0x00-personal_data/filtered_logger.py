1ff07e890da2b1745e44c0e9d3234341c132f475
NgigiN <ngigi.nyongo@gmail.com>
2024-05-30 19:15:46 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,10 +29,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Intializes the class with correct arguments"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """This method filters data using the filter_datum method"""
         original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION, original_message,
                             self.SEPARATOR)
