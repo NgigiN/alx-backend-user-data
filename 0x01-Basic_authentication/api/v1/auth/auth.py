2be3371abab99bdaeafcb3e47917ff5d3d83af17
NgigiN <ngigi.nyongo@gmail.com>
2024-06-06 17:13:43 +0300

    Authenticated routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,20 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method for basic authentication"""
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        normalized_paths = path.rstrip('/') + '/'
+        normalized_excluded_paths = [
+            ep.rstrip('/') + '/' for ep in excluded_paths]
+
+        if normalized_paths in normalized_excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method handling the authorization header"""
