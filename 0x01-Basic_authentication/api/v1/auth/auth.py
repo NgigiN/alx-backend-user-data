3da2f7d7ec1c3da0ba2839cff992d198033a01a7
NgigiN <ngigi.nyongo@gmail.com>
2024-06-06 12:23:13 +0300

    Auth Class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,17 +6,16 @@ from typing import List, TypeVar
 
 
 class Auth():
-  """Class to manage the Authentication"""
+    """Class to manage the Authentication"""
 
-  def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    """Method for basic authentication"""
-    return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method for basic authentication"""
+        return False
 
-  def authorization_header(self, request=None) -> str:
-    """Method handling the authorization header"""
-    return None
+    def authorization_header(self, request=None) -> str:
+        """Method handling the authorization header"""
+        return None
 
-  def current_user(self, request=None) -> TypeVar('User'):
-    """Method for handling users
-    """
-    return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Method for handling users"""
+        return None
