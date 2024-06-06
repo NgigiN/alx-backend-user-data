11b1772e145390e11d1c9abfb55af4cbea591aad
NgigiN <ngigi.nyongo@gmail.com>
2024-06-06 12:11:05 +0300

    Auth Class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A class to manage the API authentication"""
+
+from flask import request
+from typing import List
+
+class Auth():
+  """Class to manage the Authentication"""
+
+  def require_auth(self, path:str, excluded_paths: List[str]) -> bool:
+    """Method for basic authentication"""
+    return None
+
+
+  def authorization_header(self, request=None) -> str:
+    """Method handling the authorization header"""
+    return None
+
+
+  def current_user(self, request=None) -> TypeVar('User'):
+    """Method for handling users
+    """
+    return None
