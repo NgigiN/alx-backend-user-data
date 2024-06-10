7676685d52e538800aec332834f7fbef44bcf9f8
NgigiN <ngigi.nyongo@gmail.com>
2024-06-10 14:10:45 +0300

    User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,15 +37,13 @@ class BasicAuth(Auth):
                                  decoded_base64_authorization_header: str) -> (str, str):
         """This method returns the user email and password from the decoded value"""
         if decoded_base64_authorization_header is None:
-            return None, None
+            return (None, None)
+
         if not isinstance(decoded_base64_authorization_header, str):
-            return None, None
+            return (None, None)
 
-        decoded_bytes = base64.b64decode(decoded_base64_authorization_header)
-        decoded_str = decoded_bytes.decode('utf-8')
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
 
-        if ":" not in decoded_str:
-            return None, None
-        email, password = decoded_str.split(':', 1)
-
-        return email, password
+        credentials = decoded_base64_authorization_header.split(':', 1)
+        return (credentials[0], credentials[1])
